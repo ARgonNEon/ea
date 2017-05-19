c182d22f35ce32af0d6b59f0ad8d758300c1c20b
Arne Sachtler <Arne.Sachtler@dlr.de>
2017-05-19 18:42:37 +0200

    Implement hillclimbing demonstrator

--- src/hillclimber/hillclimber.go
+++ src/hillclimber/hillclimber.go
@@ -3,16 +3,16 @@ package hillclimber
 import "ackley"
 
 type Hillclimber struct {
-	nmax int
-	delta float64
-	dim int
+	Nmax  int
+	Delta float64
+	Dim   int
 }
 
 func (hill Hillclimber) Climb(initialValue []float64) []float64 {
-	bestpoint := make([]float64, hill.dim)
+	bestpoint := make([]float64, hill.Dim)
 	copy(bestpoint, initialValue)
 	bestval := ackley.Ackley(initialValue)
-	for i:=0; i<hill.nmax; i++ {
+	for i:=0; i<hill.Nmax; i++ {
 		c := make(chan []float64, 5)
 		go hill.calculateNeighbors(bestpoint, c)
 		for neighbor := range c {
@@ -25,12 +25,12 @@ func (hill Hillclimber) Climb(initialValue []float64) []float64 {
 	return bestpoint
 }
 
-func (hill Hillclimber) calculateNeighbors(point []float64, neighbors chan []float64) {
-	for i:=0; i<hill.dim; i++ {
+func (hill Hillclimber) calculateNeighbors(point []float64, neighbors chan<- []float64) {
+	for i:=0; i<hill.Dim; i++ {
 		for _, sign := range []float64{-1, 1} {
-			neighbor := make([]float64, hill.dim)
+			neighbor := make([]float64, hill.Dim)
 			copy(neighbor, point)
-			neighbor[i] += sign * hill.delta
+			neighbor[i] += sign * hill.Delta
 			neighbors <- neighbor
 		}
 	}
--- src/main.go
+++ src/main.go
@@ -2,9 +2,23 @@ package main
 
 import "fmt"
 import "ga"
-
+import (
+	"hillclimber"
+	"ackley"
+)
 
 func main() {
+	demonstrateHillClimber()
+}
+
+func demonstrateHillClimber() {
+	climber := hillclimber.Hillclimber{1000, 1e-1, 6}
+	result := climber.Climb([]float64{1, 1, 1, 0.5, 0.5, 0.5})
+	fmt.Println("Result: ", result)
+	fmt.Println("Value: ", ackley.Ackley(result))
+}
+
+func demonstrateGeneticAlgorithm() {
 	pop := ga.GenerateStartPopulation(50)
 	fmt.Printf("")
 	pop.Analyze()
