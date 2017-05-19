0bdcc8588c3403398c1d7c94a27cd4722ba35476
Arne Sachtler <Arne.Sachtler@dlr.de>
2017-05-20 00:04:07 +0200

    Implement dummy recombiner

--- src/ga/recombiner.go
+++ src/ga/recombiner.go
@@ -3,5 +3,8 @@ package ga
 type Recombine func(parents <-chan Individuum, children chan<- Individuum, popsize int)
 
 func DummyRecombiner(parents <-chan Individuum, children chan<- Individuum, popsize int) {
-
+	defer close(children)
+	for i:=0; i<popsize; i++ {
+		children <- <-parents
+	}
 }
