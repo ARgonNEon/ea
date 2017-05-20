59449bdd5308535168a366550d7cd739162b621c
Arne Sachtler <Arne.Sachtler@dlr.de>
2017-05-20 22:40:13 +0200

    Implement Stringer in individuum type

--- src/ga/individuum.go
+++ src/ga/individuum.go
@@ -3,6 +3,10 @@ package ga
 import "math/rand"
 import "time"
 import "ackley"
+import (
+	"bytes"
+	"fmt"
+)
 
 var discretize = NewStandardDiscretizer()
 
@@ -28,10 +32,19 @@ func MakeIndividuum(template Individuum) Individuum {
 	return make([]int, len(template))
 }
 
-func (individuum Individuum) getFitness() float64 {
+func (individuum Individuum) GetFitness() float64 {
 	values := make([]float64, 0, len(individuum))
 	for _, val := range individuum {
 		values = append(values, discretize.Code2Value(val))
 	}
 	return ackley.Ackley(values)
+}
+
+func (individuum Individuum) String() string {
+	var ss bytes.Buffer
+	ss.WriteString(fmt.Sprintf("Individuum {\n"))
+	ss.WriteString(fmt.Sprintf("\tPhenotype: %.2f\n", individuum.GetFitness()))
+	ss.WriteString(fmt.Sprintf("\tGenotype: %v\n", []int(individuum)))
+	ss.WriteString(fmt.Sprintln("}"))
+	return ss.String()
 }
